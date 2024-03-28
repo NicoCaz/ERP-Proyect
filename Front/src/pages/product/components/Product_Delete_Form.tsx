3fd59f69453bf1fbfddf81cd31cfc328c8acdf0e
Nicolas Cazorla Martinez <nicolascazorlamartinez@gmail.com>
2024-03-28 18:33:16 -0300

    zsdfbdfbg

--- Front/src/pages/product/components/Product_Delete_Form.tsx
+++ Front/src/pages/product/components/Product_Delete_Form.tsx
@@ -15,53 +15,34 @@ const Product_Delete_Form: React.FC<Product_Delete_Form_Props> = ({ product, onC
   });
 
   const { deletedProduct } = useStore();
-  const [confirmationMessage, setConfirmationMessage] = useState<string>("");
-
   useEffect(() => {
     setFormData(product);
   }, [product]);
 
-  const handleSubmit = () => {
-    setConfirmationMessage(`¿Estás seguro de eliminar el producto "${formData.Name}"?`);
-  };
 
-  const handleDeleteConfirmation = () => {
+  const handleDeleteConfirmation =async (e: React.FormEvent) =>{
+    e.preventDefault();
     deletedProduct(product);
     onClose();
   };
 
-  const handleCancel = () => {
-    setConfirmationMessage("");
+  const handleCancel = (e: React.FormEvent) => {
+    e.preventDefault();
+    onClose();
   };
+
+
 
   return (
     <form className="max-w-3xl grid gap-4 py-10 px-8 sm:grid-cols-2 bg-white rounded-md shadow-lg">
-      {confirmationMessage ? (
-        <div className="flex flex-col items-center">
-          <p className="text-red-700 mb-4">{confirmationMessage}</p>
-          <div className="flex justify-center">
-            <button
-              className="bg-red-500 text-white py-2 px-6 rounded-md hover:bg-red-700 mr-2"
-              onClick={handleDeleteConfirmation}
-            >
-              Confirmar
-            </button>
-            <button
-              className="bg-gray-500 text-white py-2 px-6 rounded-md hover:bg-gray-700 ml-2"
-              onClick={handleCancel}
-            >
-              Cancelar
-            </button>
-          </div>
-        </div>
-      ) : (
+      
         <div className="flex flex-col">
-          <p className="text-sm font-semibold text-gray-700 mb-1">Nombre: {formData.Name}</p>
+          <p className="text-sm font-semibold text-gray-700 mb-1">Desea eliminar el siguiente producto? : {formData.Name}</p>
           <div className="flex justify-center">
             <button
               type="button"
               className="mt-6 bg-red-500 text-white py-2 px-6 rounded-md hover:bg-red-700 focus:outline-none focus:ring-2 focus:ring-red-500"
-              onClick={handleSubmit}
+              onClick={handleDeleteConfirmation}
             >
               Eliminar
             </button>
@@ -74,7 +55,7 @@ const Product_Delete_Form: React.FC<Product_Delete_Form_Props> = ({ product, onC
             </button>
           </div>
         </div>
-      )}
+      
     </form>
   );
 };
